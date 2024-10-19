d25696803dd3d3dc5ba321fdd6606f825948421a
kanyasibinda <kanyasibinda.sqae@outlook.com>
2024-10-19 20:40:09 +0200

    moved the testng.xml file to resources

--- src/test/java/webuitests/UnitTests.java
+++ src/test/java/webuitests/UnitTests.java
@@ -1,7 +1,13 @@
 package webuitests;
 
 import base_tests.WebUIBaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class UnitTests extends WebUIBaseTest {
 
+    @Test
+    public void demoTest(){
+        Assert.assertTrue(true);
+    }
 }
